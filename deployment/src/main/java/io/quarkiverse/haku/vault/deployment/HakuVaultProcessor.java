1e5ccf0dec587a2768ffde3229acace0bb4bf931
zZHorizonZz <danfiala23@gmail.com>
2023-06-07 11:35:48 +0200

    Initial commit

---
+++ deployment/src/main/java/io/quarkiverse/haku/vault/deployment/HakuVaultProcessor.java
@@ -0,0 +1,14 @@
+package io.quarkiverse.haku.vault.deployment;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+
+class HakuVaultProcessor {
+
+    private static final String FEATURE = "haku-vault";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+}
